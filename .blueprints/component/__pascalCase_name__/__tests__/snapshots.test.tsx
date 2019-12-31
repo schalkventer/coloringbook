bc14e740eb57b335468f6174d4c09c56208f2b44
Schalk Venter <venterschalk@gmail.com>
2019-12-31 13:21:30 +0200

    Updates blueprint template

--- .blueprints/component/__pascalCase_name__/__tests__/snapshots.test.tsx
+++ .blueprints/component/__pascalCase_name__/__tests__/snapshots.test.tsx
@@ -2,4 +2,20 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { Default } from './stories';
 
-test('Demo', (): void => expect(renderer.create(<Default />).toJSON()).toMatchSnapshot());
+
+// ⚙️ Elements
+// 🔨 Custom
+// 📺 Views
+
+
+describe('UI Components', (): void => {
+  describe('Experimental', (): void => {
+    describe('{{pascalCase name}}', (): void => {
+      describe('snapshots', (): void => {
+        test('Default', (): void => {
+          expect(renderer.create(<Default />).toJSON()).toMatchSnapshot();
+        });
+      });
+    });
+  });
+});
